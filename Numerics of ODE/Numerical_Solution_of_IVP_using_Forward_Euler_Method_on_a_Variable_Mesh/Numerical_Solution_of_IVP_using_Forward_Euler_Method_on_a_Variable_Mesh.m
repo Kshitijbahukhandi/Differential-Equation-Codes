ec9fbb1d610d6c76268241a6b4372403149e0bc3
Kshitij <kshitijbahukhandi01@gmail.com>
2026-07-29 22:19:06 +0530

    Fix embedded repository issue

---
+++ Numerics of ODE/Numerical_Solution_of_IVP_using_Forward_Euler_Method_on_a_Variable_Mesh/Numerical_Solution_of_IVP_using_Forward_Euler_Method_on_a_Variable_Mesh.m
@@ -0,0 +1,76 @@
+        %--------------------------------
+% Name:  KSHITIJ BAHUKHANDI
+% Roll No : SAU/AM(M)/2023/06
+% APM (II-Semester)-2024
+% SOUTH  ASIAN  UNIVERSITY
+% MATLAB File:- 06
+% Numerical Solution of IVP using Forward Euler Method on a Variable Mesh 
+% Ut= -2*t*U*U
+% U(t)= 1/(1+t*t)
+%-----------------------------
+  clear all;  % clear all variables in memory
+  N=10; 
+  ETA=1.2;
+  disp('Variable Mesh Numerical Solution for IVP Using Forward Euler Method');
+  disp('    ');
+  fprintf('N=%4.2f\n',N);
+  disp('  ');
+  for I=1:8
+    % Formation of variable mesh lengths
+       SUM=1.0;
+       for L=1:N-1
+           SUM=SUM+ETA^L;
+       end
+       H(2)=1.0/SUM;
+       for L=2:N
+           H(L+1)=ETA*H(L);
+       end 
+  % Formation of grid points
+    T(1)=0.0;
+    for L=2:N+1
+          T(L)=T(L-1)+H(L);   
+    end 
+    T(N+1)=1.0;
+    % Exact Solution & RHS function
+    for L=1:N+1
+        EU(L)= 1.0/(1+T(L)*T(L));  
+    end
+    % Initial conditions
+ %   U(1)=EU(1);
+    U(1)=1.0;
+     tic  % CPU time starts
+    % Solution at advanced time
+       for L=1:N
+           H0=H(L+1);
+       U(L+1)=U(L)-2*H0*T(L)*U(L)*U(L);
+       end 
+  % Calculate Errors
+        ERU=abs(U(N+1)-EU(N+1));
+     % Print errors
+       fprintf('N=%4.2f, ERU=%10.4e\n',N,ERU);
+  disp('  ');
+  N=2*N;
+  end
+   toc  % CPU time ends
+    % Plot the results
+  figure(1);
+  plot(T,U,'--g'); 
+  xlabel('t-values');
+  ylabel('Numerical values u');
+  title('Numerical Solution')
+  figure (2);
+  plot(T,EU,'--r');
+  xlabel('t-values');
+  ylabel('Exact values u');
+  title ('Exact Solution')
+  figure (3);
+  plot(T,U,'-g',T,EU,'--r');
+  xlabel('t-values');
+  ylabel('Numerical and Exact values u');
+  title ('Exact vs Numerical Solution')
+  figure(4);
+  plot(T,U,'b+'); hold on
+  plot(T,EU,'-r'); hold off
+  xlabel('t-values');
+  ylabel('Numerical and Exact values u');
+  title('Numerical vs Exact Solution')
